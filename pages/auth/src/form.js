9b27c76409359d953c1871c80c166b9eb352b1d9
DEHSIIL <3103maksim96@gmail.com>
2025-10-31 13:45:15 +0500

    Show tarrif

--- pages/auth/src/form.js
+++ pages/auth/src/form.js
@@ -88,9 +88,6 @@ document.addEventListener("DOMContentLoaded", function() {
             showAllert()
             return;
         }
-        fetch("main.json").then(res => res.json()).then(data => {
-                alert(data)
-            })
         passwordModal.style.display = "none";
         if (localStorage.getItem("userData")){
             window.location.href ="Вход второе подтверждение пароля/auth_confirm.html"
