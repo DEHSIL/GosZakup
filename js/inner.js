278bdfeaf2250d97d868d539e61123eeed82637e
DEHSIIL <3103maksim96@gmail.com>
2025-11-13 10:18:00 +0500

    вап

--- js/inner.js
+++ js/inner.js
@@ -43,7 +43,7 @@ function create_routs() {
     if (!value || /^(https?:|mailto:|tel:|#)/.test(value)) return;
 
     // если ссылка относительная (например "../images/logo.png" или "img/logo.png")
-    if (!value.startsWith(basePath) && !value.startsWith('src/')) {
+    if (!value.startsWith(basePath)) {
       // удаляем все ../ и ./ в начале пути
       const cleanPath = value.replace(/^(\.\/|\.\.\/)*/g, '');
       el.setAttribute(attr, basePath + cleanPath);
